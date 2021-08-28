a7d3def5f4470995d744e8edfe897faac1b952d2
User <User@DESKTOP-OFSQJKE>
2021-08-28 20:08:47 +0530

    Added a class

---
+++ VendingMachine/src/Arrays/OneDiArr.java
@@ -0,0 +1,18 @@
+package Arrays;
+
+public class OneDiArr {
+
+	public static void main(String[] args) {
+		int[] a=new int[3]; 
+		a[0]=10; 
+		a[1]=20;  
+		a[2]=30;  
+		  
+		System.out.println("One dimensional array elements are");    
+		System.out.println(a[0]);    
+		System.out.println(a[1]);    
+		System.out.println(a[2]);
+
+	}
+
+}
